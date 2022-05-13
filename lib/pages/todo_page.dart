cc64459d33a3527bf0416a92c7740b9682b94c33
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-05-13 10:28:15 +0500

    search todo add

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoprovider/providers/active_todo_count.dart';
 import 'package:todoprovider/providers/filtered_todos.dart';
+import 'package:todoprovider/providers/todo_filter.dart';
 import 'package:todoprovider/providers/todo_list.dart';
+import 'package:todoprovider/providers/todo_search.dart';
+import 'package:todoprovider/utils/debounce.dart';
 import '../models/todo_model.dart';
 
 
@@ -28,7 +31,8 @@ class _TodosPageState extends State<TodosPage> {
             child: Column(
               children: [
                 TodoHeader(),
-                 SizedBox(height: 20.0),
+                  SizedBox(height: 20.0),
+                  SearchAndFilterTodo(),
                  ShowTodos(),
               ],
             ),
@@ -63,6 +67,68 @@ class TodoHeader extends StatelessWidget {
   }
 }
 
+
+
+class SearchAndFilterTodo extends StatelessWidget {
+  SearchAndFilterTodo({Key? key}) : super(key: key);
+  final debounce = Debounce(milliseconds: 1000);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Search todos',
+            border: InputBorder.none,
+            filled: true,
+            prefixIcon: Icon(Icons.search),
+          ),
+          onChanged: (String? newSearchTerm) {
+            if (newSearchTerm != null) {
+              debounce.run(() {
+                context.read<TodoSearch>().setSearchTerm(newSearchTerm);
+              });
+            }
+          },
+        ),
+        SizedBox(height: 10.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            filterButton(context, Filter.all),
+            filterButton(context, Filter.active),
+            filterButton(context, Filter.completed),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget filterButton(BuildContext context, Filter filter) {
+    return TextButton(
+      onPressed: () {
+        context.read<TodoFilter>().changeFilter(filter);
+      },
+      child: Text(
+        filter == Filter.all
+            ? 'All'
+            : filter == Filter.active
+                ? 'Active'
+                : 'Completed',
+        style: TextStyle(
+          fontSize: 18.0,
+          color: textColor(context, filter),
+        ),
+      ),
+    );
+  }
+
+  Color textColor(BuildContext context, Filter filter) {
+    final currentFilter = context.watch<TodoFilter>().state.filter;
+    return currentFilter == filter ? Colors.blue : Colors.grey;
+  }
+}
 
 class ShowTodos extends StatelessWidget {
   const ShowTodos({Key? key}) : super(key: key);
